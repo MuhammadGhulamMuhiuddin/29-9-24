81d947d36ca5d13df35124b5b587896343700bac
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-09-29 12:08:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+// Task N0.1
+
+// var taker = prompt("Enter Whatever","chicago");
+
+// var breaker = taker.split(" ");
+
+// var arr = []
+
+// for (let i = 0; i < breaker.length; i++) {
+//     var a = breaker[i].slice(0,1).toUpperCase();
+//     var b = breaker[i].slice(1);
+//     var result = a + b;
+//     console.log(result);
+    
+    
+// }
+
+
+
+// Task N0.2
+// var str = "Saylani Mass It Training Program";
+
+// for(var i = 0; i < str.length; i++){
+//     if(str.slice(i, i + 8) == "Training"){
+      
+//         alert("Found!")
+
+//     }
+// }
+
+// Task N0.3
+// a
+
+// var str = "Saylani Mass It Training Program";
+
+// for(var i = 0; i < str.length; i++){
+//     if(str.indexOf(i, i + 8) == "Training"){
+//         alert("Found!")
+//     }
+// }
+
+// b
+
+// var str = "Saylani Mass It Training"
+// var searchStr = str.indexOf("Training");
+// console.log(searchStr);
+
+
+// Task N0.4
+
+// var str = ("Hey! EveryOne I am the student of SMIT, SMIT is one of the gratest paltform where thousand of students make their future bright and at the end i really want to say that. Thanks To SMIT");
+// var anotherStr = str.replaceAll("SMIT","SayalaniMassITTraning");
+// console.log(anotherStr);
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var str = "Saylani Mass It Training Program";
+// var searchStr = "Training";
+
+// for (var i = 0; i < str.length;  i++) {
+//     if (str.indexOf(i, i + searchStr.length) == searchStr) {
+//         alert("Found!");
+//         break;  
+//     }
+// }
